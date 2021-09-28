2cea05767bed828cac495c6a051c14b4c8138226
Giovanni Picogna <picogna@usm.lmu.de>
2021-09-28 17:15:30 +0200

    first version of the website

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Influence of stellar mass on disc photoevaporation",
+        "excerpt":"The strong X-ray irradiation from young solar-type stars may play a crucial role in the thermodynamics and chemistry of circumstellar discs, driving their evolution in the last stages of disc dispersal as well as shaping the atmospheres of newborn planets. In this paper we study the influence of stellar mass...","categories": ["research paper"],
+        "tags": ["photoevaporation","stellar mass"],
+        "url": "/research%20paper/welcome-to-jekyll/",
+        "teaser": null
+      }]
