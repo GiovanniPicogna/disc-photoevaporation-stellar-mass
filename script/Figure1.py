8b744f1ff97a1e4d5ee0ab3efbb13571dcca64fd
Giovanni Picogna <picogna@usm/lmu.de>
2021-09-29 13:41:14 +0200

    changed scripts Fig. 1 and 4 to add au in the x-axis

--- script/Figure1.py
+++ script/Figure1.py
@@ -21,19 +21,36 @@ Mstars = [0.1, 0.3, 0.5, 1.0]
 
 fig = plt.figure()
 gs = fig.add_gridspec(nrows=2, ncols=4, hspace=0, wspace=0)
-ax = gs.subplots(sharex=True, sharey=True)
+ax = gs.subplots(sharex=False, sharey=True)
 
 labels = ["\\texttt{0.1Msun}", "\\texttt{0.3Msun}", "\\texttt{0.5Msun}", "\\texttt{1Msun}"]
 
 xticks = np.arange(0, 21, 5)
+xtick_label = [[0,2.5,5,7.5,10],
+          [0,8,16,24,32],
+          [0,14,28,42,56],
+          [0,25,50,75,100]]
 
 for i in range(2):
     for j in range(4):
         ax[i, j].set_xlim(0, 24)
         ax[i, j].set_ylim(0, 24)
         ax[i, j].set_aspect("equal")
+
+        Rg = 5.05 * Mstars[j]
+
         if i == 1:
-            ax[i, j].set_xlabel(r"R [R$_g$]", size=14, color="black")
+            ax[i, j].set_xlabel(r"R [R$_g$]")
+            ax[i, j].set_xticks(xticks)
+        if i == 0:
+            ax[i, j].set_title(labels[j], size=14.0)
+            ax2 = ax[i,j].twiny()
+            ax[i, j].set_xticks([])
+            ax2.set_xlim(ax[i,j].get_xlim())
+            ax2.set_xticks(np.asarray(xtick_label[j])/Rg)
+            ax2.set_xticklabels(xtick_label[j])
+            ax2.set_xlabel(r'R [au]')
+
         if j == 0:
             ax[i, j].set_ylabel(r"Z [R$_g$]", size=14, color="black")
         file = basepath + files[j]
@@ -106,8 +123,6 @@ for i in range(2):
         )
         Tdustc = Tdustc.reshape(numz, numr)
 
-        Rg = 5.05 * Mstars[j]
-
         if i == 0:
             quantity = Tdustc
             labelplot = r"T [K]"
@@ -119,10 +134,6 @@ for i in range(2):
             valmin = 3
             valmax = 11
 
-        if i == 0:
-            ax[i, j].set_title(labels[j], size=14.0)
-            ax[i, j].spines["top"].set_visible(True)
-
         plot = ax[i, j].pcolormesh(
             Xc * u.cm.to(u.AU) / Rg,
             Zc * u.cm.to(u.AU) / Rg,
@@ -132,7 +143,6 @@ for i in range(2):
             vmax=valmax,
             cmap=plt.cm.jet,
         )
-        ax[i, j].set_xticks(xticks)
 
         if j == 3:
             fig.subplots_adjust(right=0.9)
@@ -141,6 +151,5 @@ for i in range(2):
             )
             fig.colorbar(plot, ax=ax[i, j], cax=cax, label=labelplot)
 
-plt.tight_layout(pad=0.0)
 fig.savefig("Figure1.png", bbox_inches="tight", dpi=400)
 plt.close(fig)
--- script/Figure4.py
+++ script/Figure4.py
@@ -18,7 +18,7 @@ mu = 2.35
 
 fig = plt.figure()
 gs = fig.add_gridspec(nrows=2, ncols=4, hspace=0, wspace=0)
-ax = gs.subplots(sharex=True, sharey=True)
+ax = gs.subplots(sharex=False, sharey=True)
 
 dirs = [
     "01Msun",
@@ -30,6 +30,10 @@ labels = ["\\texttt{0.1Msun}", "\\texttt{0.3Msun}", "\\texttt{0.5Msun}", "\\text
 Mstar = [0.1, 0.3, 0.5, 1.0]
 index_max = [415, 470, 452, 553]
 
+xtick_label = [[0,2.5,5,7.5,10],
+          [0,8,16,24,32],
+          [0,14,28,42,56],
+          [0,25,50,75,100]]
 xticks = np.arange(0, 21, 5)
 os.chdir("../data/PLUTO/")
 for i in range(2):
@@ -38,13 +42,24 @@ for i in range(2):
         ax[i, j].set_xlim(0, 24.99)
         ax[i, j].set_ylim(0, 24.99)
         ax[i, j].set_aspect("equal")
+        Rg = 5.05 * Mstar[j]
+
         if i == 1:
-            ax[i, j].set_xlabel(r"R [R$_g$]", size=14, color="black")
+            ax[i, j].set_xlabel(r"R [R$_g$]")
+            ax[i, j].set_xticks(xticks)
+        if i == 0:
+            ax[i, j].set_title(labels[j], size=14.0)
+            ax2 = ax[i,j].twiny()
+            ax[i, j].set_xticks([])
+            ax2.set_xlim(ax[i,j].get_xlim())
+            ax2.set_xticks(np.asarray(xtick_label[j])/Rg)
+            ax2.set_xticklabels(xtick_label[j])
+            ax2.set_xlabel(r'R [au]')
         if j == 0:
             ax[i, j].set_ylabel(r"Z [R$_g$]", size=14, color="black")
 
         files = getFilenames()[:]
-        Rg = 5.05 * Mstar[j]
+        
         step = 0
         xcell, ycell, zcell = (getGridCell() * rscale).to(u.cm)
         X = xcell
@@ -52,11 +67,7 @@ for i in range(2):
         D = (getVar(files[step], step, "rho") * rhoscale).to(u.g / u.cm ** 3)
         Pr = (getVar(files[step], step, "prs") * vscale ** 2 * rhoscale).to(u.barye)
         T = ((Pr * mu * const.m_p) / (const.k_B * D)).to(u.K).value
-        vr = (getVar(files[step], step, "vx1") * vscale).to(u.cm / u.s)
-        vth = (getVar(files[step], step, "vx2") * vscale).to(u.cm / u.s)
-        cs2 = (const.G * Mstar[j] * const.M_sun / (2 * X)).to(u.cm ** 2 / u.s ** 2)
-        vel2 = vr ** 2 + vth ** 2
-        Rstart = []
+        
         if i == 0:
             quantity = T
             labelplot = r"T [K]"
@@ -68,8 +79,6 @@ for i in range(2):
             valmin = 3
             valmax = 11
         Cd = getVar(files[step], step, "cd")
-        if i == 0:
-            ax[i, j].set_title(labels[j], size=14.0)
 
         plot = ax[i, j].pcolormesh(
             (X).to(u.AU).value / Rg,
@@ -80,6 +89,7 @@ for i in range(2):
             vmax=valmax,
             cmap=plt.cm.jet,
         )
+
         sonic = np.loadtxt("sonic1.dat")
         soundx = (np.trim_zeros(sonic[:, 0]) * u.cm).to(u.AU)
         soundz = (np.trim_zeros(sonic[:, 1]) * u.cm).to(u.AU)
@@ -99,7 +109,6 @@ for i in range(2):
             linestyles="dotted",
             colors="red",
         )
-        ax[i, j].set_xticks(xticks)
 
         if j == 3:
             fig.subplots_adjust(right=0.9)
@@ -108,8 +117,6 @@ for i in range(2):
             )
             fig.colorbar(plot, ax=ax[i, j], cax=cax, label=labelplot)
         os.chdir("../")
-
-plt.tight_layout(pad=0.0)
 
 os.chdir("../../script")
 fig.savefig("Figure4.png", bbox_inches="tight", dpi=400)
